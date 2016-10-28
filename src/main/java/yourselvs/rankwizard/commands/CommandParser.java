0d7d9bf2bee4caf53f548dd1258e3b17baf9ba71
Michael <yourselvs@live.com>
2016-10-28 19:15:57 -0400

    Added initial parsers to CommandParser

--- src/main/java/yourselvs/rankwizard/commands/CommandParser.java
+++ src/main/java/yourselvs/rankwizard/commands/CommandParser.java
@@ -1,5 +1,6 @@
 package yourselvs.rankwizard.commands;
 
+import net.md_5.bungee.api.ChatColor;
 import yourselvs.rankwizard.RankWizard;
 
 public class CommandParser {
@@ -10,15 +11,49 @@ public class CommandParser {
 	}
 	
 	public void parseCommand(Cmd command) {
-		
+		switch(command.label) {
+		case "rw": parseRw(command); break;
+		case "rankup": parseRankup(command); break;
+		case "chooseclass": parseChooseclass(command); break;
+		case "rank": parseRank(command); break;
+		case "class": parseClass(command); break;
+		}
 	}
 
 	private void parseRw(Cmd command) {
+		if(command.args.length < 1) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[0]) {
+		case "rank": parseRwRank(command); break;
+		case "class": parseRwClass(command); break;
+		case "defaultclass": parseRwDefaultclass(command); break;
+		case "help": parseRwHelp(command); break;
+		default: parseInfo(command);
+		}
+	}
+	
+	private void parseInfo(Cmd command) {
+		// TODO
 	}
 	
 	private void parseRwRank(Cmd command) {
+		if(command.args.length < 2) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[1]) {
+		case "create": parseRwRankCreate(command); break;
+		case "description": parseRwRankDescription(command); break;
+		case "mainclass": parseRwRankMainclass(command); break;
+		case "forcerankup": parseRwRankForcerankup(command); break;
+		case "requirement": parseRwRankRequirement(command); break;
+		case "reward": parseRwRankReward(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwRankCreate(Cmd command) {
@@ -38,7 +73,17 @@ public class CommandParser {
 	}
 	
 	private void parseRwRankRequirement(Cmd command) {
+		if(command.args.length < 3) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[2]) {
+		case "list": parseRwRankRequirementList(command); break;
+		case "set": parseRwRankRequirementSet(command); break;
+		case "remove": parseRwRankRequirementRemove(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwRankRequirementList(Cmd command) {
@@ -46,7 +91,17 @@ public class CommandParser {
 	}
 	
 	private void parseRwRankRequirementSet(Cmd command) {
+		if(command.args.length < 4) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[3]) {
+		case "money": parseRwRankRequirementSetMoney(command); break;
+		case "item": parseRwRankRequirementSetItem(command); break;
+		case "group": parseRwRankRequirementSetGroup(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwRankRequirementSetMoney(Cmd command) {
@@ -62,7 +117,17 @@ public class CommandParser {
 	}
 	
 	private void parseRwRankRequirementRemove(Cmd command) {
+		if(command.args.length < 4) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[3]) {
+		case "money": parseRwRankRequirementRemoveMoney(command); break;
+		case "item": parseRwRankRequirementRemoveItem(command); break;
+		case "group": parseRwRankRequirementRemoveGroup(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwRankRequirementRemoveMoney(Cmd command) {
@@ -78,7 +143,17 @@ public class CommandParser {
 	}
 	
 	private void parseRwRankReward(Cmd command) {
+		if(command.args.length < 3) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[2]) {
+		case "list": parseRwRankRewardList(command); break;
+		case "set": parseRwRankRewardSet(command); break;
+		case "remove": parseRwRankRewardRemove(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwRankRewardList(Cmd command) {
@@ -86,7 +161,17 @@ public class CommandParser {
 	}
 	
 	private void parseRwRankRewardSet(Cmd command) {
+		if(command.args.length < 4) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[3]) {
+		case "money": parseRwRankRewardSetMoney(command); break;
+		case "item": parseRwRankRewardSetItem(command); break;
+		case "group": parseRwRankRewardSetGroup(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwRankRewardSetMoney(Cmd command) {
@@ -102,7 +187,17 @@ public class CommandParser {
 	}
 	
 	private void parseRwRankRewardRemove(Cmd command) {
+		if(command.args.length < 4) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[3]) {
+		case "money": parseRwRankRewardRemoveMoney(command); break;
+		case "item": parseRwRankRewardRemoveItem(command); break;
+		case "group": parseRwRankRewardRemoveGroup(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwRankRewardRemoveMoney(Cmd command) {
@@ -118,7 +213,16 @@ public class CommandParser {
 	}
 	
 	private void parseRwClass(Cmd command) {
+		if(command.args.length < 2) {
+			parseInfo(command); 
+			return;
+		}
 		
+		switch(command.args[1]) {
+		case "create": parseRwClassCreate(command); break;
+		case "description": parseRwClassDescription(command); break;
+		default: parseInfo(command);
+		}
 	}
 	
 	private void parseRwClassCreate(Cmd command) {
