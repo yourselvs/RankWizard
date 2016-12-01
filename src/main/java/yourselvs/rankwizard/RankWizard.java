80aa0db083a488fe9786696036724c26d1bf0587
Michael <yourselvs@live.com>
2016-12-01 02:17:14 -0500

    Whitespace

--- src/main/java/yourselvs/rankwizard/RankWizard.java
+++ src/main/java/yourselvs/rankwizard/RankWizard.java
@@ -7,7 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
